3557f1674af9df165b28cb292c3f79f1dbb5be7e
Dai Bui <daib@Mary-Stewarts-MacBook-Pro.local>
2011-03-18 03:04:30 -0700

    add parsing credit packets + generate R plot file.

--- stat248/R/parse.pl
+++ stat248/R/parse.pl
@@ -5,6 +5,9 @@ use strict;
 my $trace = "trace";
 my $map = "map.txt";
 my $taskGraph = "task_graph.txt";
+my $action = "send";
+my $R = "plot.R";
+my $graph = "graphs";
 
 #mao from thread to ip
 my %threadIP = ();
@@ -30,6 +33,11 @@ while(<MAP>)
 
 close MAP;
 
+open R, ">$R" or die $!;
+
+print R "postscript(\"$graph.ps\")\npar(mfrow=c(2,1))\n";
+
+
 open TRACE, $trace or die $!;
 
 open TASKGRAPH, $taskGraph or die $!;
@@ -40,26 +48,52 @@ while(<TASKGRAPH>)
     {
         my $src = $threadIP{$1};
         my $dst = $threadIP{$2};
-        print "$1 $src $2 $dst\n";
+        print "$1\[$src\] $2\[$dst\]\n";
 
         seek(TRACE, 0, 0);
 
-        my $flow = "s$1-$2";
+        my $flow = "s$1_$2";
         
         open FLOW,">$flow" or die $!;
 
         while(<TRACE>)
         {
-            if(/Time (\d+) node $src sends (\d+) to $dst/)
+            if(/Time\s+(\d+)\s+node\s+$src\s+$action\s+(\d+)\s+to\s+$dst/)
+            {
+                print FLOW "$1 $2\n";
+            }
+        }
+       
+        print R "$flow = diff(read.table(\"$flow\")[,1])\n"; 
+        print R "plot($flow, type=\"l\")\n";
+
+        close FLOW;
+
+        $flow = "s$2_$1";
+        
+        open FLOW,">$flow" or die $!;
+
+        seek(TRACE, 0, 0);
+
+        while(<TRACE>)
+        {
+            if(/Time\s+(\d+)\s+node\s+$dst\s+$action\s+(\d+)\s+to\s+$src/)
             {
                 print FLOW "$1 $2\n";
             }
         }
         
+        print R "$flow = diff(read.table(\"$flow\")[,1])\n"; 
+        print R "plot($flow, type=\"l\")\n";
+
         close FLOW;
     }
 }
 
 close TASKGRAPH;
+
+close R;
+
 close TRACE;
 
+system "ps2pdf $graph.ps $graph.pdf; open $graph.pdf";
