d23e30165e5481f45cff4b28b7f996d2156dd201
Dai Bui <daib@Mary-Stewarts-MacBook-Pro.local>
2011-03-20 19:00:23 -0700

    adding routing and analysis for sharing flows on each link.

--- stat248/R/parse.pl
+++ stat248/R/parse.pl
@@ -2,12 +2,132 @@
 
 use strict;
 
+###### Routing section
+my $nX = 3;
+my $nY = 3;
+
+my $north = 1;
+my $south = 2;
+my $east = 3;
+my $west = 4;
+
+my $currentNode = -1;
+
+my $routing = "network_config";
+
+my $nodeSig = "\@NODE";
+my $tableSig = "routing_scheme table";
+
+my %routingTable = ();
+
+
+open ROUTING, $routing or die $!;
+
+while(<ROUTING>)
+{
+    if(/$nodeSig\s+(\d+)/)
+    {
+        $currentNode = $1;         
+        $routingTable{$currentNode} = [];
+        #print "Current node $currentNode \n";
+    } 
+
+
+    if(/$tableSig\s+(.*)/)
+    {
+        while(/\s+(\d+)(.*)/)
+        {
+            #print "\t$1\n";
+            push (@{$routingTable{$currentNode}}, $1);
+            $_ = $2;
+        } 
+    }
+}
+
+close ROUTING;
+
+sub flowLinks
+{
+    my $src = $_[0]; 
+    my $dst = $_[1];
+    my @links = qw();
+    
+    my $currentPos = $src;
+    
+    while($currentPos ne $dst)
+    {
+        my $nextPos = -1;
+        #next pos
+        if($routingTable{$currentPos} ne undef)
+        {
+            #default XY routing
+            my $currentX = $currentPos / $nY;
+            my $currentY = $currentPos % $nY;
+
+            my $dstX = $dst / $nY
+            my $dstY = $dst % $nY;
+
+            if($currentX > $dstX)
+            {
+                $currentX--;     
+            }
+            else if($currentX < $dstX)
+            {
+                $currentX++;
+            }
+            else if($currentY > $dstY)
+            {
+                $currentY--;
+            }
+            else if($currentY < $dstY)
+            {
+                $currentY++;
+            }
+            
+            $nextPos = $currentX * $nY + $currentY;
+        }
+        else
+        {
+            #table routing
+            my $dir = $routingTable{$currentPos}[$dst];
+            if($dir == $north)
+            {
+                $nextPos = $currentPos + 1;
+            }
+            if($dir == $south)
+            {
+                $nextPos = $currentPos - 1;
+            }
+            if($dir == $east)
+            {
+                $nextPos = $currentPos + $nY;
+            }
+            if($dir == $west)
+            {
+                $nextPos = $currentPos - $nY;
+            }
+        }
+        
+        push(@links, "$currentPos\_$nextPos");
+
+        $currentPos = $nextPos;
+    }
+
+    return @links;
+}
+
+###### Parsing section
+
 my $trace = "trace";
 my $map = "map.txt";
 my $taskGraph = "task_graph.txt";
 my $action = "sends";
 my $R = "plot.R";
 my $graph = "graphs";
+
+
+#map from links to set of flows going through the links
+%linkFlows = ();
 
 #map from thread to ip
 my %threadIP = ();
@@ -46,7 +166,7 @@ while(<TASKGRAPH>)
     {
         my $src = $threadIP{$1};
         my $dst = $threadIP{$2};
-        my $bufSize = $3;
+        my $bufSize = $3 * 4;
         my $currentSent = 0;
 
         print "$1\[$src\] $2\[$dst\]\n";
@@ -64,7 +184,8 @@ while(<TASKGRAPH>)
                 $currentSent += ($2 - 1); 
                 if($currentSent >= $bufSize)
                 {
-                    print FLOW "$1 $currentSent\n";
+                    #print FLOW "$1 $currentSent\n";
+                    print FLOW "$1\n";
                     $currentSent = 0;
                 }
             }
@@ -72,6 +193,17 @@ while(<TASKGRAPH>)
        
         print R "$flow = diff(read.table(\"$flow\")[,1])\n"; 
         print R "plot($flow, type=\"l\")\n";
+
+        #compute links on a path
+        foreach my $link ( &flowLinks($src, $dst))
+        {
+            if($linkFlows{$link} eq undef)
+            {
+                $linkFlows{$link} = [];
+            }
+            
+            push (@{$linkFlows{$link}}, $flow);
+        }
 
         close FLOW;
 
@@ -85,12 +217,23 @@ while(<TASKGRAPH>)
         {
             if(/Time\s+(\d+)\s+node\s+$dst\s+$action\s+(\d+)\s+to\s+$src/)
             {
-                print FLOW "$1 $2\n";
+                print FLOW "$1\n";
             }
         }
         
         print R "$flow = diff(read.table(\"$flow\")[,1])\n"; 
         print R "plot($flow, type=\"l\")\n";
+
+        #compute links on a path
+        foreach my $link ( &flowLinks($dst, $src))
+        {
+            if($linkFlows{$link} eq undef)
+            {
+                $linkFlows{$link} = [];
+            }
+            
+            push (@{$linkFlows{$link}}, $flow);
+        }
 
         close FLOW;
     }
