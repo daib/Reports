8b1239451ae9c3904c56b35f558b7dd0c5b5cc0f
Dai Bui <daib@Mary-Stewarts-MacBook-Pro-3.local>
2011-09-21 19:24:57 -0700

    implemented mincut. Checking for error.

--- CS294-69/imgquilting/quilting.m
+++ CS294-69/imgquilting/quilting.m
@@ -3,8 +3,8 @@ function [ qim ] = quilting( im, pW, pH, sW, sH )
 
 [iH,iW,iB] = size(im);
 
-overlappingW  = floor(pW / 10);
-overlappingH = floor(pH / 10);
+overlappingW  = floor(pW / 5);
+overlappingH = floor(pH / 5);
 
 
 qim = zeros(sH, sW, iB);
@@ -22,12 +22,12 @@ for j = floor(1:sH/(pH - overlappingH))
         else
             %search for a suitable block that is close to neighboring patched
             %blocks
-            min = 0;
+            minVal = 0;
             minH = 0;
             minW = 0;
             
             % iterate through the input image
-            % to find the suitable match
+            % to find the most suitable match
             for w = 1:(iW - pW)
                 for h = 1:(iH - pH)
                     tmpPatch = im((h:(h+pH-1)), (w:(w+pW-1)), :);
@@ -44,13 +44,13 @@ for j = floor(1:sH/(pH - overlappingH))
                         error = error + overllappingError(tmpPatch((1:overlappingH) , : , :), qim(initPoitH:(initPoitH + overlappingH - 1), (initPoitW:(initPoitW + pW - 1)), :));
                     end
                     
-                    if min == 0
-                        min = error;
+                    if minVal == 0
+                        minVal = error;
                         minH = h;
                         minW = w;
                     else
-                        if min > error
-                            min = error;
+                        if minVal > error
+                            minVal = error;
                             minH = h;
                             minW = w;
                         end
@@ -62,6 +62,25 @@ for j = floor(1:sH/(pH - overlappingH))
             
             % find the minimum cut
             
+            if j > 1
+                % horizontal cut
+                hCurve = mincut(patch((1:overlappingH) , : , :), qim(initPoitH:(initPoitH + overlappingH - 1), (initPoitW:(initPoitW + pW - 1)), :), true);
+                
+                % copy the image
+                for w = 1:pW
+                    patch(1:hCurve(w), w, :) = qim(initPoitH:(initPoitH + hCurve(w) - 1), (initPoitW + w - 1), :);
+                end
+            end
+            
+            if i > 1
+                % vertical cut
+                vCurve = mincut(patch(: , (1:overlappingW), :), qim(initPoitH:(initPoitH + pH - 1), (initPoitW:(initPoitW + overlappingW - 1)), :), false);
+                
+                % copy the image
+                for h = 1:pH
+                    patch(h, 1:vCurve(h), :) = qim((initPoitH + h - 1), initPoitW:(initPoitW + vCurve(h) - 1), :);
+                end
+            end
             
         end
         qim(initPoitH:(initPoitH + pH - 1), initPoitW:(initPoitW + pW - 1), :) = patch;
